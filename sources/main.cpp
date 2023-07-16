5dda03cc7fbe03f1b9f40a0b03c5e7171fc7e049
trainzkid <trainzkid@gmail.com>
2023-07-16 13:11:34 -0500

    Tidying up to maintain consistency

--- sources/main.cpp
+++ sources/main.cpp
@@ -11,7 +11,7 @@ namespace LineNum {
 	void linenum(std::istream &in,std::ostream &out) {
 		std::string line;
 		signed short int lineCount=1;
-		signed short int NROFF_LINE_LIMIT = 66;
+		signed short int NROFF_LINE_LIMIT=66;
 
 		while(std::getline(in,line)) {
 			out<<std::format("{:2}:",lineCount++)<<line<<std::endl;
@@ -28,7 +28,7 @@ int main(int arg_count,char* arg_array[]) {
 		LineNum::linenum(std::cin,std::cout);
 	else {
 		for(std::vector<std::string>::const_iterator i=args.begin()+1;i!=args.end();++i) {
-			const std::string arg = *i;
+			const std::string arg=*i;
 			std::fstream file(arg,std::ios::in);
 
 			if(!file.is_open()) {		// is this even necessary?
@@ -39,6 +39,5 @@ int main(int arg_count,char* arg_array[]) {
 
 		}
 	}
-
 	return 0;
 }
