73b0e1bcca90fec5021d6c492add2b302155c9ec
trainzkid <trainzkid@gmail.com>
2023-07-17 15:26:51 -0500

    std::string_view supremacy!!! almost..

--- sources/main.cpp
+++ sources/main.cpp
@@ -22,13 +22,13 @@ namespace LineNum {
 }
 
 int main(int arg_count,char* arg_array[]) {
-	const std::vector<std::string> args(arg_array,arg_array+arg_count);
+	const std::vector<std::string_view> args(arg_array,arg_array+arg_count);
 
 	if(args.size()<=1)
 		LineNum::linenum(std::cin,std::cout);
 	else {
-		for(std::vector<std::string>::const_iterator i=args.begin()+1;i!=args.end();++i) {
-			const std::string arg=*i;
+		for(std::vector<std::string_view>::const_iterator i=args.begin()+1;i!=args.end();++i) {
+			const std::string arg(*i);
 			std::fstream file(arg,std::ios::in);
 
 			if(!file.is_open()) {		// is this even necessary?
